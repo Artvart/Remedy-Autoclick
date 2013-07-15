175462fe71c2495e1701e74f311afd95ec6ce055
Artvart <d.artvrt@gmail.com>
2013-07-15 05:00:20 +0400

    Changed to Alex Ivanov

--- Remedy Automation/Form1.Designer.cs
+++ Remedy Automation/Form1.Designer.cs
@@ -38,7 +38,14 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -54,20 +61,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 137);
+            this.label1.Location = new System.Drawing.Point(229, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Pos_X";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(73, 137);
+            this.label2.Location = new System.Drawing.Point(306, 86);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Pos_Y";
             // 
             // button2
             // 
@@ -75,8 +82,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(218, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Expand";
+            this.button2.Text = "Expand/Minimize";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // notifyIcon1
@@ -113,15 +121,68 @@
                 "wn create\nF10 - BCCH down autofill\n\nLeftAlt + F8 - Get cursor pos (for debug)\nby" +
                 " Voronetsky Dm.";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.richTextBox2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(240, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(375, 198);
+            this.panel1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(229, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(232, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Update config";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(215, 197);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(232, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "positions.cfg";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 217);
+            this.ClientSize = new System.Drawing.Size(238, 217);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -131,8 +192,9 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -146,6 +208,12 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Remedy Automation/Form1.cs
+++ Remedy Automation/Form1.cs
@@ -8,53 +8,59 @@ using System.Text;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace Remedy_Automation
 {
     public partial class Form1 : Form
     {
-		//winapi global hotkeys for creating actions. Unregistrated on app close
+        //winapi global hotkeys for creating actions. Unregistrated on app close
         [DllImport("user32.dll")]
         private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
         [DllImport("user32.dll")]
         private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-		//Find remedy handler for later control.
-	    [DllImport("user32.dll", SetLastError = true)]
+        //Find remedy handler for later control.
+        [DllImport("user32.dll", SetLastError = true)]
         public static extern IntPtr FindWindow(string className, string windowTitle);
         [DllImport("user32.dll", SetLastError = true)]
         public static extern IntPtr FindWindowEx(IntPtr hWnd, IntPtr hwndChild, string className, string windowTitle);
         //Window position detection
-		[DllImport("user32.dll")]
+        [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
         //Cursor go to X,Y
-		[DllImport("user32.dll")]
+        [DllImport("user32.dll")]
         static extern bool SetCursorPos(int X, int Y);
-		//Cursor click
-		[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        //Cursor click
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
-		
+
         [DllImport("user32.dll")]
         static extern bool SetForegroundWindow(IntPtr hWnd);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool Beep(int frequency, int duration);
-		       
+
         // Hotkeys optional keys
         public const int MOD_ALT = 1;
         public const int MOD_CONTROL = 2;
         public const int MOD_SHIFT = 4;
         public const int MOD_WIN = 8;
-		// Hotkeys messages for respond
+        // Hotkeys messages for respond
         public const int WM_HOTKEY1 = 0x312;
         public const int WM_HOTKEY2 = 0x313;
         public const int WM_HOTKEY3 = 0x314;
         public const int WM_HOTKEY4 = 0x315;
         public const int WM_HOTKEY5 = 0x316;
-		// Mouse buttons
+        // Mouse buttons
         static int WM_LBUTTONDOWN = 0x02;
         static int WM_LBUTTONUP = 0x04;
-        
+
+        Stream sw;
+
+        int[] coordinates_X = new int[58];
+
         bool Expand_flag = true;
 
         public Form1()
@@ -66,9 +72,62 @@ namespace Remedy_Automation
             RegisterHotKey(this.Handle, WM_HOTKEY3, MOD_ALT, (int)Keys.F8);
             RegisterHotKey(this.Handle, WM_HOTKEY4, 0, (int)Keys.F9);
             RegisterHotKey(this.Handle, WM_HOTKEY5, 0, (int)Keys.F10);
+
+            //try
+            //{
+            //    richTextBox2.LoadFile("pos.ini", RichTextBoxStreamType.PlainText);
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.ToString(), ex.Source);
+            //    MessageBox.Show("Configuration file not found.");
+            //}
+
+            coordinates_X[1] = 50;
+            coordinates_X[2] = 305;
+            coordinates_X[4] = 137;
+            coordinates_X[5] = 245;
+            coordinates_X[7] = 482;
+            coordinates_X[8] = 312;
+            coordinates_X[10] = 469;
+            coordinates_X[11] = 432;
+            coordinates_X[13] = 664;
+            coordinates_X[14] = 621;
+            coordinates_X[16] = 228;
+            coordinates_X[17] = 695;
+            coordinates_X[19] = 782;
+            coordinates_X[20] = 625;
+            coordinates_X[22] = 499;
+            coordinates_X[23] = 746;
+            coordinates_X[24] = 494;
+            coordinates_X[25] = 742;
+            coordinates_X[27] = 314;
+            coordinates_X[28] = 901;
+            coordinates_X[29] = 314;
+            coordinates_X[30] = 911;
+            coordinates_X[32] = 50;
+            coordinates_X[33] = 305;
+            coordinates_X[35] = 137;
+            coordinates_X[36] = 544;
+            coordinates_X[38] = 463;
+            coordinates_X[39] = 402;
+            coordinates_X[40] = 345;
+            coordinates_X[41] = 795;
+            coordinates_X[42] = 493;
+            coordinates_X[43] = 557;
+            coordinates_X[45] = 482;
+            coordinates_X[46] = 312;
+            coordinates_X[48] = 469;
+            coordinates_X[49] = 432;
+            coordinates_X[51] = 782;
+            coordinates_X[52] = 625;
+            coordinates_X[54] = 314;
+            coordinates_X[55] = 901;
+            coordinates_X[56] = 314;
+            coordinates_X[57] = 911;
         }
 
-		//Rectangle structure for window position IN/OUT
+        //Rectangle structure for window position IN/OUT
         public struct RECT
         {
             public int Left;        // x position of upper-left corner
@@ -79,21 +138,23 @@ namespace Remedy_Automation
 
         static void Click(int x, int y)
         {
+            Thread.Sleep(100);
             SetCursorPos(x, y);
             mouse_event(WM_LBUTTONDOWN | WM_LBUTTONUP, 0, 0, 0, 0);
         }
 
         static void DoubleClick(int x, int y)
         {
+            Thread.Sleep(100);
             SetCursorPos(x, y);
             mouse_event(WM_LBUTTONDOWN | WM_LBUTTONUP, 0, 0, 0, 0);
             mouse_event(WM_LBUTTONDOWN | WM_LBUTTONUP, 0, 0, 0, 0);
         }
-        
+
         Rectangle myRect = new Rectangle();
         RECT rct;
 
-		//Hotkey rections here
+        //Hotkey rections here
         protected override void WndProc(ref Message m)
         {
             //Hotkey check.
@@ -112,9 +173,9 @@ namespace Remedy_Automation
                     myRect.Y = rct.Top;
 
                     //Создать по шаблону
-                    Click(myRect.X+50, myRect.Y+305);
-					//Откючение питания
-                    DoubleClick(myRect.X + 137, myRect.Y + 245);
+                    Click(myRect.X + 34, myRect.Y + 253);
+                    //Откючение питания
+                    DoubleClick(myRect.X + 309, myRect.Y + 167);
                     //Кликнуть в элемент сети.
                     Click(myRect.X + 482, myRect.Y + 312);
                     //Кликнуть в поле ввода
@@ -124,29 +185,26 @@ namespace Remedy_Automation
                     MainHwnd = FindWindow("ArFrame", "BMC Remedy User - [Инцидент (Новый)]");
                     if (!GetWindowRect(MainHwnd, out rct))
                     {
-						//%Username%, ты делаешь что-то не так
+                        //%Username%, ты делаешь что-то не так
                         MessageBox.Show("Remedy not found. Mb it's not in an acidents screen?");
                         return;
                     }
                     myRect.X = rct.Left;
                     myRect.Y = rct.Top;
                     //Прокликиваем какой-то шлак
-                    Click(myRect.X + 2344-1680, myRect.Y + 621);
+                    Click(myRect.X + 670, myRect.Y + 627);
                     //Выделение  Радиоподсистема -> Прочие. 
                     Click(myRect.X + 228, myRect.Y + 695);
-					InputLanguage.CurrentInputLanguage =  InputLanguage.FromCulture(new System.Globalization.CultureInfo("ru-RU"));
                     System.Threading.Thread.Sleep(500);
+                    //Try this
+                    //InputLanguage.CurrentInputLanguage =  InputLanguage.FromCulture(new System.Globalization.CultureInfo("ru-RU"));
                     SendKeys.Send("Прочие");
-                    //3
-                    Click(myRect.X + 2462-1680, myRect.Y + 625);
-                    //4
-                    Click(myRect.X + 2179-1680, myRect.Y + 746);
-                    //5
-                    Click(myRect.X + 2174-1680, myRect.Y + 742);
+
+                    Click(myRect.X + 782, myRect.Y + 625);
+                    Click(myRect.X + 499, myRect.Y + 746);
+                    Click(myRect.X + 494, myRect.Y + 742);
                     //изменить масштаб влияния на 1
-                    //6
                     Click(myRect.X + 314, myRect.Y + 901);
-                    //7
                     Click(myRect.X + 314, myRect.Y + 911);
                     break;
                 case WM_HOTKEY3:
@@ -160,41 +218,32 @@ namespace Remedy_Automation
                         MessageBox.Show(MainHwnd.ToString());
                         return;
                     }
-                    
+
                     myRect.X = rct.Left;
                     myRect.Y = rct.Top;
 
                     //Mouse Move to create incident link
-                    //1
-                    Click(myRect.X+50, myRect.Y+305);
-                    //2
-                    DoubleClick(myRect.X + 137, myRect.Y + 544);
-                    //3
+
+                    //Создать по шаблону
+                    Click(myRect.X + 34, myRect.Y + 253);
+                    DoubleClick(myRect.X + 128, myRect.Y + 310);
                     Click(myRect.X + 463, myRect.Y + 402);
-                    //4
                     Click(myRect.X + 345, myRect.Y + 795);
-                    //5
                     Click(myRect.X + 493, myRect.Y + 557);
                     //Кликнуть в элемент сети.
-                    //IntPtr MainHwnd = FindWindow("ArFrame", "BMC Remedy User - [Инцидент (Новый)]");
-                    //6
                     Click(myRect.X + 482, myRect.Y + 312);
                     //Кликнуть в поле ввода
-                    //7
                     Click(myRect.X + 469, myRect.Y + 432);
                     break;
                 case WM_HOTKEY5:
-                    //1
-                    Click(myRect.X + 2462-1680, myRect.Y + 625);
-                    //2
+                    Click(myRect.X + 775, myRect.Y + 626);
                     Click(myRect.X + 314, myRect.Y + 901);
-                    //3
                     Click(myRect.X + 314, myRect.Y + 911);
                     break;
             }
             base.WndProc(ref m);
         }
-        
+
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -237,7 +286,7 @@ namespace Remedy_Automation
         {
             if (Expand_flag)
             {
-                this.Width = 700;
+                this.Width = 633;
                 Expand_flag = false;
             }
             else
@@ -245,7 +294,35 @@ namespace Remedy_Automation
                 this.Width = 245;
                 Expand_flag = true;
             }
-
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            return;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //textBox1.Text = listBox1.Items[listBox1.SelectedIndex].ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //File.WriteAllLines("pos.ini", listBox1.Items.Cast<string>().ToArray());
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < richTextBox2.Lines.Count(); i++)
+            {
+                int num = 0;
+                bool pars = int.TryParse(richTextBox2.Lines[i], out num);
+                if (pars)
+                {
+                    coordinates_X[i] = num;
+                }
+            }
+        }
+
     }
 }
